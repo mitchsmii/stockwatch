b6738138f7e822b34654b098adc13bb944b53f28
mitchsmii <143842296+mitchsmii@users.noreply.github.com>
2025-08-15 17:18:12 -0600

    idk

--- src/middleware.ts
+++ src/middleware.ts
@@ -73,8 +73,7 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * - public files (images, etc.)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 } 
